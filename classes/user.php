8bb17c84d7fdfad72d6dda59b556d1e0927845dd
elamir <amirrezatm@protonmail.com>
2024-12-17 22:33:58 +0330

    Database connection retrying logic added

--- classes/user.php
+++ classes/user.php
@@ -2,30 +2,61 @@
     class User {
         public function create(int $telegram_user_id) {
             global $pdo;
-            $stmt = $pdo->prepare("INSERT INTO `users` (`telegram_user_id`" .
-            ") VALUES (:tui);");
-            $stmt->execute(array(':tui' => $telegram_user_id));
+            for ($i=0; $i < 10; $i++) {
+                try {
+                    $stmt = $pdo->prepare("INSERT INTO `users` (`telegram_user_id`" .
+                    ") VALUES (:tui);");
+                    $stmt->execute(array(':tui' => $telegram_user_id));
+                    break;
+                } catch(Exception $e) {
+                    echo $e->getMessage();
+                    db_up();
+                }
+            }
         }
 
         public function update($column, $value, $variable_name, $assigned_value) {
             global $pdo;
-            $stmt = $pdo->prepare("UPDATE `users` 
-                       SET `$variable_name` = :asv 
-                       WHERE `$column` = :ui");
-            $stmt->execute(array(':ui' => $value,
-                        ':asv' => $assigned_value));
+            for ($i=0; $i < 10; $i++) {
+                try {
+                    $stmt = $pdo->prepare("UPDATE `users` 
+                            SET `$variable_name` = :asv 
+                            WHERE `$column` = :ui");
+                    $stmt->execute(array(':ui' => $value,
+                                ':asv' => $assigned_value));
+                        break;
+                } catch(Exception $e) {
+                    echo $e->getMessage();
+                    db_up();
+                }
+            }
         }
 
         public function read($column, $value) {
             global $pdo;
-            return $pdo->query("SELECT * FROM `users` WHERE $column=$value")->fetchAll()[0];
+            for ($i=0; $i < 10; $i++) {
+                try {
+                    return $pdo->query("SELECT * FROM `users` WHERE $column=$value")->fetchAll()[0];
+                } catch(Exception $e) {
+                    echo $e->getMessage();
+                    db_up();
+                }
+            }
         }
 
         public function flush($user_id) {
             global $pdo;
-            $stmt = $pdo->prepare('UPDATE `users` SET `checkpoint` = "", `background` = NULL,' .
-            '`text_one` = NULL, `text_two` = NULL, `text_three` = NULL, `text_four` = NULL,' .
-            '`text_five` = NULL WHERE `telegram_user_id` = :si;');
-            $stmt->execute(array(':si' => $user_id));
+            for ($i=0; $i < 10; $i++) {
+                try {
+                    $stmt = $pdo->prepare('UPDATE `users` SET `checkpoint` = "", `background` = NULL,' .
+                    '`text_one` = NULL, `text_two` = NULL, `text_three` = NULL, `text_four` = NULL,' .
+                    '`text_five` = NULL WHERE `telegram_user_id` = :si;');
+                    $stmt->execute(array(':si' => $user_id));
+                    break;
+                } catch(Exception $e) {
+                    echo $e->getMessage();
+                    db_up();
+                }
+            }
         }
     }
--- modules/db.php
+++ modules/db.php
@@ -1,13 +1,20 @@
 <?php
-    //Variables are derived from .env in bootstrap.php
-    $pdo = new PDO("mysql:host=$DB_HOST;port=$DB_PORT", $DB_USER, $DB_PASS);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    $create_db = $pdo->prepare("CREATE DATABASE IF NOT EXISTS $DB_NAME");
-    $create_db->execute();
-    
-    $pdo = new PDO("mysql:host=$DB_HOST;port=$DB_PORT;dbname=$DB_NAME", $DB_USER, $DB_PASS);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    function db_up() {
+        //Variables are derived from .env in bootstrap.php
+        global $DB_HOST, $DB_PORT, $DB_USER, $DB_PASS, $DB_NAME;
+        $pdo = new PDO("mysql:host=$DB_HOST;port=$DB_PORT", $DB_USER, $DB_PASS);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_PERSISTENT, True);
+        
+        $create_db = $pdo->prepare("CREATE DATABASE IF NOT EXISTS $DB_NAME");
+        $create_db->execute();
+        
+        $pdo = new PDO("mysql:host=$DB_HOST;port=$DB_PORT;dbname=$DB_NAME", $DB_USER, $DB_PASS);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_PERSISTENT, True);
+    }
+
+    db_up();
 
     try {
 
@@ -31,6 +38,3 @@
     } catch (PDOException $e) {
         echo "Error: " . $e->getMessage();
     }
-
-    //CREATE DATABASE pishulizebot;
-    //GRANT ALL PRIVILEGES ON pishulizebot.* TO 'pishulize_admin'@'%' IDENTIFIED BY stuff;
